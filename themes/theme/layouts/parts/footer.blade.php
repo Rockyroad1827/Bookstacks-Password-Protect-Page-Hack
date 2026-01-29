99a8044f6e4d231f0e7be7e53ad3f0eeddb368e1
Daniel Dixon <112588576+Rockyroad1827@users.noreply.github.com>
2026-01-29 11:32:12 +0000

    Refactor footer and cleanup protected tag handling

--- themes/theme/layouts/parts/footer.blade.php
+++ themes/theme/layouts/parts/footer.blade.php
@@ -1,7 +1,6 @@
 <div class="print-hidden">
     <footer class="px-xl py-m mt-xl border-top text-muted text-small">
         <div class="container">
-            <p>&copy; BookStack</p>
         </div>
     </footer>
 
@@ -60,7 +59,6 @@
 <div class="print-hidden">
     <footer class="px-xl py-m mt-xl border-top text-muted text-small">
         <div class="container">
-            <p>&copy; BookStack</p>
         </div>
     </footer>
 
